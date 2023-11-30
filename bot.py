9a658c0dd0c7c627c7a07243221f93771dbed37a
simon38514258 <78457309+simon38514258@users.noreply.github.com>
2023-11-30 15:03:57 +0800

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,59 @@
+import os
+import asyncio
+import discord
+import json
+from discord.ext import commands
+
+#讀取json檔
+with open("setting.json","r",encoding="utf8")as jfile:
+    jdata = json.load(jfile)
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix = jdata["CommandPrefix"], intents = intents)
+
+# 當機器人完成啟動時
+@bot.event
+async def on_ready():
+    print(f"目前登入身份：{bot.user}")
+
+@bot.event
+#當機器人完成啟動時
+async def on_ready():
+    print('目前登入身份：', bot.user)
+    game = discord.Game('窩ㄞ咩咩')#暱稱欄位文字
+    #discord.Status.<狀態>，可以是online,offline,idle,dnd,invisible
+    await bot.change_presence(status=discord.Status.online, activity=game)
+
+# 載入指令程式檔案
+@bot.command()
+async def load(ctx, extension):
+    await bot.load_extension(f"cogs.{extension}")
+    await ctx.send(f"Loaded {extension} done.")
+
+# 卸載指令檔案
+@bot.command()
+async def unload(ctx, extension):
+    await bot.unload_extension(f"cogs.{extension}")
+    await ctx.send(f"UnLoaded {extension} done.")
+
+# 重新載入程式檔案
+@bot.command()
+async def reload(ctx, extension):
+    await bot.reload_extension(f"cogs.{extension}")
+    await ctx.send(f"ReLoaded {extension} done.")
+
+# 一開始bot開機需載入全部程式檔案
+async def load_extensions():
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
+            await bot.load_extension(f"cogs.{filename[:-3]}")
+            print(filename)
+
+async def main():
+    async with bot:
+        await load_extensions()
+        await bot.start(jdata["TOKEN"])
+
+# 確定執行此py檔才會執行
+if __name__ == "__main__":
+    asyncio.run(main())
