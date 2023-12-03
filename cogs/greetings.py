21bee66d485344bdb4a63e897eba1313e2dc3fad
simon38514258 <78457309+simon38514258@users.noreply.github.com>
2023-12-04 01:15:39 +0800

    Update greetings.py

--- cogs/greetings.py
+++ cogs/greetings.py
@@ -6,19 +6,20 @@ from core.classes import Cog_Extension
 with open("setting.json","r",encoding="utf8")as jfile:
     jdata = json.load(jfile)
 class gretings(Cog_Extension):
-
+  
     @commands.Cog.listener()
     async def on_member_join(self, member):
         guild_id = int(jdata["Guild"]) 
         if member.guild.id == guild_id:
-            channel = self.bot.get_channel(int(jdata["Member_channel"]))
-            await channel.send(f"{member.mention}歡迎參觀咩咩教堂")
+            channel = self.bot.get_channel(int(jdata["GreetingsChannelId"]))
+            AnnouncementChannel = self.bot.get_channel(int(jdata["AnnouncementChannelId"]))
+            await channel.send(f"{member.mention}歡迎參觀咩咩教堂\n⁠記得去{AnnouncementChannel.mention}看一下噢")
     
     @commands.Cog.listener()
     async def on_member_remove(self,member):
         guild_id = int(jdata["Guild"]) 
         if member.guild.id == guild_id:
-            channel = self.bot.get_channel(int(jdata["Member_channel"]))
+            channel = self.bot.get_channel(int(jdata["GreetingsChannelId"]))
             await channel.send(f"{member.mention}離開咩咩身邊惹")
 
 async def setup(bot):
